14fd3b6c94ff2aa227684b03ddfb6b4b9dc03375
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-08-09 20:16:21 -0300

    create tables owners and species

--- schema.sql
+++ schema.sql
@@ -8,7 +8,16 @@ CREATE TABLE animals (
 	neutered BOOLEAN,
 	weight_kg DECIMAL,
 	PRIMARY KEY(id)
-)
+);
 
-ALTER TABLE animals
-ADD species VARCHAR(25);
+ALTER TABLE animals ADD species VARCHAR(25);
+
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY, 
+	full_name VARCHAR, 
+	age INT
+);
+
+CREATE TABLE species (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR);
