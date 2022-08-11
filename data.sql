d10caba27e2429ec9b21863d16951eb52325c4fa
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-08-11 20:13:59 -0300

    Insert vets into vets table

--- data.sql
+++ data.sql
@@ -92,3 +92,29 @@ UPDATE animals
 UPDATE animals
 	SET owner_id=5
 	WHERE name IN ('Angemon', 'Boarmon');
+
+-- Insert Vets into Vets table
+
+INSERT INTO vets (
+    name, age, date_of_graduation
+) VALUES (
+    'William Tatcher', 45, '2000-04-23'
+);
+
+INSERT INTO vets (
+    name, age, date_of_graduation
+) VALUES (
+    'Maisy Smith', 26, '2019-01-17'
+);
+
+INSERT INTO vets (
+    name, age, date_of_graduation
+) VALUES (
+    'Stephanie Mendez', 64, '1981-05-04'
+);
+
+INSERT INTO vets (
+    name, age, date_of_graduation
+) VALUES (
+    'Jack Harkness', 38, '2008-06-08'
+);
